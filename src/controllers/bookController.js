4d7e14eebd69e6bde571ac47ec018752d3fe865c
shohedul350 <shohedul350@gmail.com>
2020-12-28 22:06:58 +0600

    added book controller

---
+++ src/controllers/bookController.js
@@ -0,0 +1,37 @@
+import asyncHandler from '../utils/asyncHandler';
+import {
+  addBookService,
+  getBookService,
+  updateBookService,
+  deleteBookService,
+  checkTempBookService,
+} from '../services/bookServices';
+import { NotFound } from '../utils/generalError';
+
+export const addBook = asyncHandler(async (req, res) => {
+  const newBook = await addBookService(req.body);
+  return res.status(201).json({ newBook, msg: 'Book Added successfully' });
+});
+
+export const getBook = asyncHandler(async (req, res) => {
+  const book = await getBookService(req.query);
+  return res.status(200).json({ book, msg: 'Book fetch successfully' });
+});
+
+export const updateBook = asyncHandler(async (req, res) => {
+  const book = checkTempBookService(req.params.id);
+  if (!book || book.tempDeleted === true) {
+    throw new NotFound('Book not found');
+  }
+  const updatedBook = await updateBookService(req.params.id, req.body);
+  return res.status(200).json({ updatedBook, msg: 'Book update successfully' });
+});
+
+export const deleteBook = asyncHandler(async (req, res) => {
+  const book = await checkTempBookService(req.params.id);
+  if (!book || book.tempDeleted === true) {
+    throw new NotFound('Book not found');
+  }
+  const deletedBook = await deleteBookService(req.params.id);
+  return res.status(200).json({ deletedBook, msg: 'Book delete successfully' });
+});

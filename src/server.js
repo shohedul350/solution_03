5fe9ac88e8300a71ab1444f4cda6baaf864688fc
shohedul350 <shohedul350@gmail.com>
2020-12-28 11:38:30 +0600

    mongoDB connect using mongoose ODM

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import app from './app';
+import connectDb from './connectDB';
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Library server running in ${process.env.NODE_ENV} on port ${PORT}`);
+  // database connect after server run
+  connectDb();
 });

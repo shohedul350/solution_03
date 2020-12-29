d60239bf03562a60df18d4c419d4556ef9732038
shohedul350 <shohedul350@gmail.com>
2020-12-29 14:40:14 +0600

    added response message

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,12 +10,12 @@ import { NotFound } from '../utils/generalError';
 
 export const addUser = asyncHandler(async (req, res) => {
   const newUser = await addUserService(req.body);
-  return res.status(201).json({ newUser, msg: 'User create successfully' });
+  return res.status(201).json({ newUser, msg: `${newUser.role} create successfully` });
 });
 
 export const getUser = asyncHandler(async (req, res) => {
   const user = await getUserService(req.params.number);
-  return res.status(200).json({ user, msg: 'User fetch successfully' });
+  return res.status(200).json({ user, msg: `${user.role} fetch successfully` });
 });
 
 export const updateUser = asyncHandler(async (req, res) => {
@@ -24,14 +24,14 @@ export const updateUser = asyncHandler(async (req, res) => {
     throw new NotFound('User not found');
   }
   const updatedUser = await updateUserService(req.params.id, req.body);
-  return res.status(200).json({ updatedUser, msg: 'User update successfully' });
+  return res.status(200).json({ updatedUser, msg: `${updateUser.role} update successfully` });
 });
 
 export const deleteUser = asyncHandler(async (req, res) => {
   const user = await checkTempUserService(req.params.id);
   if (!user || user.tempDeleted === true) {
-    throw new NotFound('User not found');
+    throw new NotFound(`User not found in this id:${req.params.id}`);
   }
   const deletedUser = await deleteUserService(req.params.id);
-  return res.status(200).json({ deletedUser, msg: 'User delete successfully' });
+  return res.status(200).json({ deletedUser, msg: `${deleteUser.role} delete successfully` });
 });
